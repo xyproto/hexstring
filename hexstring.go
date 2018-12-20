995df32ffbd1dc587ff396ef1f4f7a40713e35f0
Alexander F. Rødseth <rodseth@gmail.com>
2018-12-20 12:09:49 +0100

    Add a function, tests and comments

--- hexstring.go
+++ hexstring.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+// HexStringToBytes takes a space separated string of hexadecimal bytes (like "7f")
+// and converts them to a slice of bytes.
 func HexStringToBytes(hexstring string) ([]byte, error) {
 	hexStrings := strings.Split(hexstring, " ")
 	hexBytes := make([]byte, 0, len(hexStrings))
@@ -14,9 +16,22 @@ func HexStringToBytes(hexstring string) ([]byte, error) {
 		}
 		u, err := strconv.ParseUint(s, 16, 8)
 		if err != nil {
-			return []byte{}, err
+			return hexBytes, err
 		}
 		hexBytes = append(hexBytes, uint8(u))
 	}
 	return hexBytes, nil
+}
+
+// BytesToHexString converts a slice of bytes to a space sparated string
+// of hexidecimal bytes (like "7f").
+func BytesToHexString(bs []byte) string {
+	var sb strings.Builder
+	for i, b := range bs {
+		if i > 0 {
+			sb.WriteString(" ")
+		}
+		sb.WriteString(strconv.FormatUint(uint64(b), 16))
+	}
+	return sb.String()
 }
--- hexstring_test.go
+++ hexstring_test.go
@@ -12,3 +12,12 @@ func ExampleHexStringToBytes() {
 	fmt.Println(helloWorldBytes)
 	// Output: [72 101 108 108 111 32 87 111 114 108 100 33]
 }
+
+func ExampleBytesToHexString() {
+	helloWorldBytes, err := HexStringToBytes("48 65 6c 6c 6f 20 57 6f 72 6c 64 21")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(BytesToHexString(helloWorldBytes))
+	// Output: 48 65 6c 6c 6f 20 57 6f 72 6c 64 21
+}
